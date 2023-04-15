68a99359461ac9b5d1bd73356c7c33223500ea47
Ramick10 <130806194+Ramick10@users.noreply.github.com>
2023-04-15 08:01:32 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let Hand = 0
 input.onButtonPressed(Button.A, function () {
     music.setVolume(115)
     for (let index = 0; index < 2; index++) {
@@ -77,7 +78,7 @@ input.onButtonPressed(Button.A, function () {
     music.playTone(415, music.beat(BeatFraction.Half))
     music.playTone(370, music.beat(BeatFraction.Breve))
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.AB, function () {
     for (let index = 0; index < 10; index++) {
         images.createBigImage(`
             # . # . . # # . . .
@@ -86,5 +87,190 @@ input.onButtonPressed(Button.B, function () {
             . # # # # # . # . .
             . # . . # . # . . .
             `).scrollImage(1, 400)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    music.setTempo(212)
+    music.playTone(659, music.beat(BeatFraction.Half))
+    music.playTone(659, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(659, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(784, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    for (let index = 0; index < 1; index++) {
+        music.playTone(523, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        music.playTone(494, music.beat(BeatFraction.Whole))
+        music.playTone(466, music.beat(BeatFraction.Half))
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        music.playTone(392, music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Sixteenth))
+        music.playTone(659, music.beat(BeatFraction.Half))
+        music.playTone(659, music.beat(BeatFraction.Sixteenth))
+        music.playTone(784, music.beat(BeatFraction.Half))
+        music.playTone(784, music.beat(BeatFraction.Sixteenth))
+        music.playTone(880, music.beat(BeatFraction.Whole))
+        music.playTone(659, music.beat(BeatFraction.Half))
+        music.playTone(784, music.beat(BeatFraction.Half))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(659, music.beat(BeatFraction.Whole))
+        music.playTone(523, music.beat(BeatFraction.Half))
+        music.playTone(587, music.beat(BeatFraction.Half))
+        music.playTone(494, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Half))
+    }
+    for (let index = 0; index < 1; index++) {
+        music.rest(music.beat(BeatFraction.Whole))
+        music.playTone(784, music.beat(BeatFraction.Half))
+        music.playTone(740, music.beat(BeatFraction.Half))
+        music.playTone(698, music.beat(BeatFraction.Half))
+        music.playTone(622, music.beat(BeatFraction.Whole))
+        music.playTone(659, music.beat(BeatFraction.Half))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(415, music.beat(BeatFraction.Half))
+        music.playTone(440, music.beat(BeatFraction.Half))
+        music.playTone(523, music.beat(BeatFraction.Half))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(440, music.beat(BeatFraction.Half))
+        music.playTone(523, music.beat(BeatFraction.Half))
+        music.playTone(587, music.beat(BeatFraction.Half))
+        music.rest(music.beat(BeatFraction.Whole))
+        music.playTone(784, music.beat(BeatFraction.Half))
+        music.playTone(740, music.beat(BeatFraction.Half))
+        music.playTone(698, music.beat(BeatFraction.Half))
+        music.playTone(622, music.beat(BeatFraction.Whole))
+        music.playTone(659, music.beat(BeatFraction.Half))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(784, music.beat(BeatFraction.Whole))
+        music.playTone(784, music.beat(BeatFraction.Half))
+        music.playTone(784, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Whole))
+        music.playTone(784, music.beat(BeatFraction.Half))
+        music.playTone(740, music.beat(BeatFraction.Half))
+        music.playTone(698, music.beat(BeatFraction.Half))
+        music.playTone(622, music.beat(BeatFraction.Whole))
+        music.playTone(659, music.beat(BeatFraction.Half))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(415, music.beat(BeatFraction.Half))
+        music.playTone(440, music.beat(BeatFraction.Half))
+        music.playTone(523, music.beat(BeatFraction.Half))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(440, music.beat(BeatFraction.Half))
+        music.playTone(523, music.beat(BeatFraction.Half))
+        music.playTone(587, music.beat(BeatFraction.Half))
+        music.rest(music.beat(BeatFraction.Whole))
+        music.playTone(622, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(587, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(523, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Double))
+    }
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Double))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.playTone(587, music.beat(BeatFraction.Half))
+    music.playTone(659, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Breve))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Double))
+    music.playTone(659, music.beat(BeatFraction.Half))
+    music.playTone(659, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(659, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(784, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+})
+input.onGesture(Gesture.Shake, function () {
+    Hand = randint(1, 3)
+    if (Hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Square,
+        3054,
+        1716,
+        167,
+        0,
+        285,
+        SoundExpressionEffect.Vibrato,
+        InterpolationCurve.Linear
+        ), SoundExpressionPlayMode.UntilDone)
+    } else {
+        if (Hand == 2) {
+            basic.showLeds(`
+                # # . . #
+                # # . # .
+                . . # . .
+                # # . # .
+                # # . . #
+                `)
+            music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 1003, 4213, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+        } else {
+            basic.showLeds(`
+                # # # # #
+                # . . . #
+                # . . . #
+                # . . . #
+                # # # # #
+                `)
+            music.playSoundEffect(music.createSoundEffect(
+            WaveShape.Sine,
+            3143,
+            1895,
+            255,
+            0,
+            500,
+            SoundExpressionEffect.None,
+            InterpolationCurve.Linear
+            ), SoundExpressionPlayMode.UntilDone)
+        }
     }
 })
